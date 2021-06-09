1ee1125017c6a0319ba4b8cec8de0e1af91346b6
byrocket09 <83572660+byrocket09@users.noreply.github.com>
2021-06-09 04:11:12 +0200

    09/06/2021 Mechanics
    hgfh

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    private bool inventoryEnabled;
+
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    public GameObject slotHandler;
+
+    void Start()
+    {
+        allSlots = slotHandler.transform.childCount;
+
+        slot = new GameObject[allSlots];
+
+        for(int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHandler.transform.GetChild(i).gameObject;
+        }
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+
+        }
+
+        if (inventoryEnabled)
+        {
+            inventory.SetActive(true);
+        }
+        else{
+
+
+            inventory.SetActive(false);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag=="item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+
+            Item item = itemPickedUp.GetComponent<Item>();
+
+            Additem(itemPickedUp,item.ID,item.type,item.descripcion,item.icon);
+
+        }
+
+    }
+
+    public void Additem(GameObject itemObject,int itemID,string itemType,string itemDescription, Sprite itemIcon)
+    {
+        
+        for(int i = 0; i < allSlots; i++)
+        {
+
+
+        }
+
+    }
+}
